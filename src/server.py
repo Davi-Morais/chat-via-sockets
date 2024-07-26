fbb3eadb562f6438a612e48346a4e3783e14f711
Davi Morais <davifarias2433@gmail.com>
2024-07-26 11:34:27 -0300

    Refatora server para usar sintaxe de classe
    A sintaxe de classe sera usada no servidor para facilitar o compartilhamento
    de dados entre as funcionalidades

--- src/server.py
+++ src/server.py
@@ -3,37 +3,39 @@ import threading
 import sys
 
 
-def handle_client(connection, client_address):
-    print(f'connection from {client_address}')
+class Server:
 
-    try:
-        while (data := connection.recv(2048)):
-            msg = data.decode('utf-8')
-            print(f"{msg}")
-            
-    
-    finally:
-        connection.close()
-        print(f"Connection with {client_address} closed!")
+    def handle_client(self, connection, client_address):
+        print(f'connection from {client_address}')
 
-
-def server(host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    address = (host, int(port))
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(address)
-
-    sock.listen(10)
-
-    print(f"Server running in {address[0]}:{address[1]}")
+        try:
+            while (data := connection.recv(2048)):
+                msg = data.decode('utf-8')
+                print(f"{msg}")
+                
+        
+        finally:
+            connection.close()
+            print(f"Connection with {client_address} closed!")
 
 
-    print('waiting connections...')
-    while True:
-        connection, client_address = sock.accept()
+    def start(self, host, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        threading.Thread(target=handle_client, args=(connection, client_address), daemon=True).start()
+        address = (host, int(port))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(address)
+
+        sock.listen(10)
+
+        print(f"Server running in {address[0]}:{address[1]}")
+
+
+        print('waiting connections...')
+        while True:
+            connection, client_address = sock.accept()
+
+            threading.Thread(target=self.handle_client, args=(connection, client_address), daemon=True).start()
 
 
 if __name__ == "__main__":
@@ -42,4 +44,5 @@ if __name__ == "__main__":
         print("Usage: server.py host port")
         exit(1)
     else:
-        server(sys.argv[1], sys.argv[2])
+        server = Server()
+        server.start(sys.argv[1], sys.argv[2])
