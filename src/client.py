22b34902069e2be17514da2e58e9cdc5cd7eb1d8
Davi Morais <davifarias2433@gmail.com>
2024-07-31 15:20:31 -0300

    Refatorando client
    A funcionalidade de enviar mensagens esta em uma thread
    separada, e a de receber mensagens do servidor na thread
    main.

--- src/client.py
+++ src/client.py
@@ -3,9 +3,14 @@ import sys
 import threading
 
 
-def recv_msg(sock):
-    while (msg := sock.recv(2048)):
-        print(msg.decode())
+def read_input(sock):
+    while (msg := input()):
+        if msg != 'exit':
+            sock.sendall(msg.encode())
+        else:
+            sock.shutdown(socket.SHUT_RDWR)
+            sock.close()
+        
 
 def client(host, port):
     
@@ -13,19 +18,18 @@ def client(host, port):
     server_address = (host, int(port))
 
     sock.connect(server_address)
+    print(f'connecting to {server_address[0]} port {server_address[1]}')
 
-    threading.Thread(target=recv_msg, args=(sock,), daemon=True).start()
+    thread = threading.Thread(target=read_input, args=(sock,), daemon=True)
+    thread.start()
 
     try:
-
-        print(f'connecting to {server_address[0]} port {server_address[1]}')
-
-        while (msg := input('> ')) != 'exit':
-            sock.sendall(msg.encode())
+        while (msg := sock.recv(2048)):
+            print(msg.decode())
 
     finally:
-        sock.close()
         print("Connection closed!")
+        exit(0)
 
 
 if __name__ == "__main__":
